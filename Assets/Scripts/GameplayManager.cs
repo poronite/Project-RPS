6b48be6397d856c8a6343a9601645b3632f66f7e
poronite <zazafonseca@gmail.com>
2020-11-01 13:40:06 +0000

    Gameplay 0.1
    Create basic Movement System

---
+++ Assets/Scripts/GameplayManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameplayManager : MonoBehaviour
+{
+    public int PlayerTurn;
+    public GameObject Player1;
+    public GameObject Player2;
+
+    void Start()
+    {
+        DetermineFirstToPlay();
+    }
+
+    private void DetermineFirstToPlay()
+    {
+        PlayerTurn = Random.Range(1, 3);
+        ChangePlayerTurn(PlayerTurn);
+        Debug.Log($"Player{PlayerTurn} goes first.");
+    }
+
+    public void ChangePlayerTurn(int PTurnTemp)
+    {
+        PlayerTurn = PTurnTemp;
+
+        if (PlayerTurn == 1)
+        {
+            Player1.GetComponent<Player>().enabled = true;
+            Player2.GetComponent<Player>().enabled = false;
+            Player1.GetComponent<Player>().numberMovesLeft = 2;
+        }
+
+        if (PlayerTurn == 2)
+        {
+            Player1.GetComponent<Player>().enabled = false;
+            Player2.GetComponent<Player>().enabled = true;
+            Player2.GetComponent<Player>().numberMovesLeft = 2;
+        }
+
+        Debug.Log($"It's Player{PlayerTurn}'s turn.");
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int numberMovesLeft;
+    private Vector2 playerPosition;
+    public int PlayerID;
+    public GameplayManager GameplayManager;
+
+
+    void Start()
+    {
+        playerPosition = gameObject.transform.position;
+    }
+
+    void Update()
+    {
+        if (GameplayManager.PlayerTurn == PlayerID && numberMovesLeft > 0)
+        {
+            Movement();
+        }
+    }
+
+    private void Movement()
+    {
+        if (Input.GetKeyDown("w"))
+        {
+            playerPosition.y += 1;
+            numberMovesLeft -= 1;
+        }
+
+        if (Input.GetKeyDown("a"))
+        {
+            playerPosition.x -= 1;
+            numberMovesLeft -= 1;
+        }
+
+        if (Input.GetKeyDown("s"))
+        {
+            playerPosition.y -= 1;
+            numberMovesLeft -= 1;
+        }
+
+        if (Input.GetKeyDown("d"))
+        {
+            playerPosition.x += 1;
+            numberMovesLeft -= 1;
+        }
+
+        gameObject.transform.position = playerPosition;
+
+        if (numberMovesLeft == 0)
+        {
+            switch (PlayerID)
+            {
+                case 1:
+                    GameplayManager.ChangePlayerTurn(2);
+                    break;
+
+                case 2:
+                    GameplayManager.ChangePlayerTurn(1);
+                    break;
+
+                default:
+                    break;
+            }
+        }
+    }
+}
