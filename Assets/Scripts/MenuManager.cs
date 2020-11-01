c47ed01f4459510af0616e9488d0a32a4def8709
poronite <zazafonseca@gmail.com>
2020-11-01 11:58:45 +0000

    Main Menu 0.1
    Creation of the buttons of the Main Menu with basic functions.

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject RPSLogo;
+    public GameObject PlayButton;
+    public GameObject SettingsButton;
+    public GameObject CreditsButton;
+    public GameObject ExitGameButton;
+
+
+    void Start()
+    {
+        MainMenu();
+    }
+
+    public void MainMenu()
+    {
+        Debug.Log("Going to Main Menu.");
+        RPSLogo.SetActive(true);
+        PlayButton.SetActive(true);
+        SettingsButton.SetActive(true);
+        CreditsButton.SetActive(true);
+        ExitGameButton.SetActive(true);
+    }
+
+    public void Play()
+    {
+        Debug.Log("Singleplayer or Multiplayer?");
+    }
+
+    public void Settings()
+    {
+        Debug.Log("Going to Settings Menu.");
+    }
+
+    public void Credits()
+    {
+        Debug.Log("Going to Credits.");
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("Exit Game");
+        Application.Quit();
+    }
+}
